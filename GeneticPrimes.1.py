8c953af27742f75d71bc0c03829671dc077d1eaf
Jaden <jadtayl@nuevaschool.org>
2016-03-01 09:15:59 -0800

    Updated 'lucky prime' function

--- GeneticPrimes.1.py
+++ GeneticPrimes.1.py
@@ -3,6 +3,8 @@ import random, sys, os, tempfile, subprocess
 
 generation = 1
 
+import time
+
 class Quadratic:
     def __init__(self, square, linear, constant):
         self.square = square
@@ -17,7 +19,7 @@ class Quadratic:
 
 gen=[0]*100*generation
 sub=[0]*100*generation
-quadsVars=[1,2,3]
+quadsVars=[]
 quads=[0] * generation
 
 fileName=os.path.basename(__file__)
@@ -28,11 +30,19 @@ if (generation>1):
     ID0[0]=str(int(ID0[0])-1)
 
     quadReference="Quads."+".".join(ID0)+".txt"
-    print quadReference
     storePath="/Users/jadtayl/Desktop/Programming/Genetic-Primes/Quadratic Storage/"+quadReference
-    storeEx=open(storePath)
+    with open(storePath) as storeEx:
+        quadsVars=storeEx.read()
+    quadsVars=quadsVars.strip("[").strip("]").replace(" ",'')
+    quadsVars=quadsVars.split(",")
 
-#quads[generation-1]=
+for x in quadsVars:
+    quadsVars[quadsVars.index(x)]=int(x)
+while len(quadsVars) < (generation*3):
+    quadsVars.append(random.randint(0,100))
+for x in xrange(generation):
+    quads[x]=Quadratic(quadsVars[x],quadsVars[x+1],quadsVars[x+2])
+
 def primeChecker(n):
     n=abs(n)
     for i in range(2,int(n**0.5)+1):
@@ -40,12 +50,12 @@ def primeChecker(n):
             return False
     return True
 
-for i in range(1,generation+1):
+for i in xrange(1,generation+1):
     lgb=i*100
     smb=(i-1)*100
     for x in range(smb,lgb):
-        gen[x-1]=(x*x)-x+41
-        #sub[x-1]=quads[i](x)
+        gen[x-1]=(x*x)+x+41
+        sub[x-1]=quads[i-1](x)
 
 genlength=len(gen)-1
 sublength=len(sub)-1
@@ -71,24 +81,33 @@ def cloneFunction():
     newtext=open(__file__,"r").read()
     newgen=str(generation+1)
     dirpath = "/Users/jadtayl/Desktop/Programming/Genetic-Primes/PrimeFunctions/Gen "+newgen+"/"
-    newtext=newtext[:91]+newgen+newtext[92:]
+    newtext=newtext[:91]+newgen+newtext[newtext.find('\n',91):]
 
     store="/Users/jadtayl/Desktop/Programming/Genetic-Primes/Quadratic Storage/Quads"+fileName[13:-3]+".txt"
     quadStorage = open(store, "w")
     quadStorage.write(str(quadsVars))
     quadStorage.close()
 
-
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
 
     for x in xrange(int(functValue*100)):
         identity=str(x+1)
-        name=dirpath+fileName[:14]+newgen+fileName[15:-2]+identity+".py"
-
+        name=dirpath+fileName[:14]+newgen+fileName[fileName.find('.',14):-2]+identity+".py"
         newFunction=open(name,"w")
         newFunction.write(newtext)
         newFunction.close()
-        #subprocess.Popen(["python", name])
-cloneFunction()
-print functValue
+        subprocess.Popen(["python", name])
+
+        time.sleep(10)
+
+if generation<5:
+    cloneFunction()
+else:
+    dataPath = "/Users/jadtayl/Desktop/Programming/Genetic-Primes/PrimeFunctions/Data/"
+    if not os.path.exists(dataPath):
+        os.makedirs(dataPath)
+    dataFile = dataPath+"Functionality="+str(functValue)+" Identity="+fileName[14:-3]+".txt"
+    dataStore = open(dataFile, "w")
+    dataStore.write("Identifier: "+fileName[14:-3]+"\n\nQuadratic Coefficients: "+str(quadsVars)+"\n\nFunctionality Value: "+str(functValue))
+    sys.exit()
